5c8e02f07c5a3b75483bedcd1e355f42a2e0691d
manana <manana>
2016-10-27 11:32:02 +0200

    bootstrap

--- javascript/Ejercicios/Ejercicio_JQuery_Ajax/js/inicio.js
+++ javascript/Ejercicios/Ejercicio_JQuery_Ajax/js/inicio.js
@@ -1,45 +1,25 @@
 $(document).ready(initializeEvents);
 function initializeEvents(){
    $("#boton1").click(guardarDato);
-   $("#boton2").click(modificarDato);
-   $("#boton3").click(borrarDato);
+   //$("#boton2").click(modificarDato);
+   //$("#boton3").click(borrarDato);
 }
 
 function guardarDato(){
-    
-}
-function modificarDato(){
-    
-}
-function borrarDato(){
-    
-}
-
-
-$(document).ready(initializeEvents);
-function initializeEvents(){
-    $("#peticion_ajax").click(peticionAjaxGenerica);
-}
-function peticionAjaxGenerica(){
     $.ajax({
-        //Puede ser una cadena, un array o un object de Js
-        //?nombre=Jonathan&nivel_de_cafe=bajo
-        data: {
-            nombre:"Jonathan", 
-            nivel_de_cafe:"bajo"
-        },
-        //Tipo de peticion http
-        type:"GET",
-        //Tipo de dato esperado 
+        type:"GET", 
         dataType:"json",
-        //URL de comunicacion con el servicio
         url:"peliculas.json"
     }).done(peticionCompletada).fail(peticionFallida);
 }
-function peticionCompletada(data, status,jqXHR){
-    alert("Peticion completada con status " +status+" : "+data);
-    $("#contenido_de_ajax").html(data[7].username);
+
+function peticionCompletada(data){
+    console.log(data.peliculas.length);
+    $("tbody").html("<tr><td>"+data.peliculas[0].id+"</td><td>"+data.peliculas[0].titulo+
+    "</td><td>"+data.peliculas[0].director+"</td><td>"+data.peliculas[0].sinopsis+
+    "</td><td>"+data.peliculas[0].fecha+"</td></tr>");
 }
+
 function peticionFallida(jqXHR,status,error){
     alert("Error al procesar la peticion");
     console.log("Status: " +status);
