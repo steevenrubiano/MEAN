a0015fc4fc992d88bb65796dff9c371c0d27476a
manana <manana>
2016-10-27 13:50:42 +0200

    act

--- javascript/Ejercicios/Ejercicio_JQuery_Ajax/js/inicio.js
+++ javascript/Ejercicios/Ejercicio_JQuery_Ajax/js/inicio.js
@@ -1,19 +1,22 @@
 $(document).ready(initializeEvents);
 function initializeEvents(){
    $("#boton1").click(guardarDato);
-   //$("#boton2").click(modificarDato);
-   //$("#boton3").click(borrarDato);
 }
+
 
 function guardarDato(){
     $.ajax({
-        type:"GET", 
+        type:"POST", 
         dataType:"json",
-        url:"peliculas.json"
-    }).done(peticionCompletada).fail(peticionFallida);
+        url:"http://localhost:3000/peliculas"
+    }).done(peticionCompletada)/*.fail(peticionFallida)*/;
 }
 
 function peticionCompletada(data){
+    console.log(data);
+}
+
+/*function peticionCompletada(data,status,jqXHR){
     console.log(data.peliculas.length);
     $("tbody").html("<tr><td>"+data.peliculas[0].id+"</td><td>"+data.peliculas[0].titulo+
     "</td><td>"+data.peliculas[0].director+"</td><td>"+data.peliculas[0].sinopsis+
@@ -24,4 +27,4 @@ function peticionFallida(jqXHR,status,error){
     alert("Error al procesar la peticion");
     console.log("Status: " +status);
     console.log("Error! "+error);
-}
+}*/
