360693b54eb75ed48ffac29ce651c00f86b6a82a
manana <manana>
2016-11-03 13:13:02 +0100

    segundo ejemplo angular

--- javascript/ejemplo06-angular2/ejemplo-02-angular/src/app/app.module.ts
+++ javascript/ejemplo06-angular2/ejemplo-02-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MiComponenteNuevoComponent } from './mi-componente-nuevo/mi-componente-nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponenteNuevoComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +17,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,MiComponenteNuevoComponent]
 })
 export class AppModule { }
