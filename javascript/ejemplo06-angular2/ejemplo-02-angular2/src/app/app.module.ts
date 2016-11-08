e32efa6dfdc027dad166a14b60447016a9f87389
manana <manana>
2016-11-08 11:34:36 +0100

    act

--- javascript/ejemplo06-angular2/ejemplo-02-angular2/src/app/app.module.ts
+++ javascript/ejemplo06-angular2/ejemplo-02-angular2/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { MiComponenteNuevoComponent } from './mi-componente-nuevo/mi-componente-
 import { Ejemplo01NgforNgifComponent } from './ejemplo01-ngfor-ngif/ejemplo01-ngfor-ngif.component';
 import { NumerosPrimosComponent } from './numeros-primos/numeros-primos.component';
 import { NumerosPrimosConServicioComponent } from './numeros-primos-con-servicio/numeros-primos-con-servicio.component';
+import { NumerosPrimosLeyendaComponent } from './numeros-primos-leyenda/numeros-primos-leyenda.component';
+import { NumerosPrimosFilaComponent } from './numeros-primos-fila/numeros-primos-fila.component';
+import { NumerosPrimosPanelComponent } from './numeros-primos-panel/numeros-primos-panel.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { NumerosPrimosConServicioComponent } from './numeros-primos-con-servicio
     MiComponenteNuevoComponent,
     Ejemplo01NgforNgifComponent,
     NumerosPrimosComponent,
-    NumerosPrimosConServicioComponent
+    NumerosPrimosConServicioComponent,
+    NumerosPrimosLeyendaComponent,
+    NumerosPrimosFilaComponent,
+    NumerosPrimosPanelComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +29,6 @@ import { NumerosPrimosConServicioComponent } from './numeros-primos-con-servicio
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent,NumerosPrimosConServicioComponent]
+  bootstrap: [AppComponent,NumerosPrimosLeyendaComponent]
 })
 export class AppModule { }
