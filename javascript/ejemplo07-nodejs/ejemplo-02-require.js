4e3b878b08a3842c0f4f8c010da1117c9c8e87c5
manana <manana>
2016-11-10 13:42:03 +0100

    act

--- javascript/ejemplo07-nodejs/ejemplo-02-require.js
+++ javascript/ejemplo07-nodejs/ejemplo-02-require.js
@@ -1,1 +1,5 @@
 let fs = require('fs');
+let fichero = process.argv[2];
+let contenido = fs.readFileSync(fichero);
+let numFilas = contenido.toString().split("\n").length;
+console.log("Fichero: " + fichero + "\nLineas: " + numFilas);
