ff60b670107b96c8afd3175753e61e3398ae99bf
manana <manana>
2016-11-17 13:47:04 +0100

    act

--- javascript/ejemplo08-express/index.js
+++ javascript/ejemplo08-express/index.js
@@ -4,25 +4,21 @@ function evaluaElVerboHttp(request,response){
     response.send("Ruta: " + request.originalUrl + 
     " Verbo :" + request.method)
 }
-app.get("/",
-    (request,response)=>{
-        console.log("Acceso a la peticion ",request);
-        response.send("Respuesta recibida!");
-    }
-);
+app.get("/", (request,response)=>{
+    console.log("Acceso a la peticion ",request);
+    response.send("Respuesta recibida!");
+});
 app.post("/",evaluaElVerboHttp);
 app.put("/",evaluaElVerboHttp);
 app.delete("/",evaluaElVerboHttp);
 app.all("/paratodos",evaluaElVerboHttp);
 app.all("/iniciofin",evaluaElVerboHttp);
-app.get("/clientes/:idCliente?*/facturas/:idFactura",
-    (request,response)=>{
-        response.send("Resultados : \n\tCliente: " + 
-        request.params.idCliente + " \n\tFactura: " + 
-        request.params.idFactura + "\n" +
-         JSON.stringify(request.params));
-    }
-);
+app.get("/clientes/:idCliente?*/facturas/:idFactura", (request,response)=>{
+    response.send("Resultados : \n\tCliente: " + 
+    request.params.idCliente + " \n\tFactura: " + 
+    request.params.idFactura + "\n" +
+    JSON.stringify(request.params));
+});
 app.get("/save/:fichero.:extension",(request,response)=>{
     response.send("Ahora genero un fichero de tipo : " + 
     request.params.extension);
@@ -33,11 +29,7 @@ function funcionIntermedia(request,response,next){
 }
 function funcionIntermediaFin(request,response,next){
     console.log("Ejecutado a las: "  + new Date());
-    //if(noAutenticado){
         response.send("fin");
-    //}else{
-    //    next()
-    //}
 }
 app.get("/concatenado",
     funcionIntermedia,
@@ -46,18 +38,15 @@ app.get("/concatenado",
     (request,response)=>{
         response.send("Enviado");
     });
-
 app.route('/rutaconjunta')
     .get(evaluaElVerboHttp)
     .post(evaluaElVerboHttp)
     .put(evaluaElVerboHttp)
     .delete(evaluaElVerboHttp);
-
 var router = express.Router();
 router.use(funcionIntermedia);
 router.get('/conrouter', evaluaElVerboHttp);
 router.post('/conrouterpost', evaluaElVerboHttp);
 app.use('/cosacuca', router);
-
 app.listen(8080);
 console.log("Servidor iniciado");
