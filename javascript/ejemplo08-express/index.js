04a1d6be97e2e7501c169b1f76417312e40dcbc5
manana <manana>
2016-11-18 10:58:19 +0100

    act

--- javascript/ejemplo08-express/index.js
+++ javascript/ejemplo08-express/index.js
@@ -48,5 +48,41 @@ router.use(funcionIntermedia);
 router.get('/conrouter', evaluaElVerboHttp);
 router.post('/conrouterpost', evaluaElVerboHttp);
 app.use('/cosacuca', router);
+
+//Ejemplo Coches
+var routerRest = express.Router();
+routerRest.route("/coches")
+    .get((request, response)=>{
+        response.json([{_id:1, marca: "opel", modelo:"corsa"},
+                        {_id:2, marca: "audi", modelo:"a4"},
+                        {_id:3, marca: "seat", modelo:"cordoba"}
+        ]);
+    })
+    .post((request, response)=>{
+        //recoger infromacion del body para crear un nuevo coches del tipo:
+        //request.body.marca
+        //request.body.modelo
+        response.json({message:"insertar"});
+    })
+routerRest.route("/coches/:idCoche")
+    .get((request, response)=>{
+        //TODO: Obtener el coche a partir de su id
+        response.json({_id:1, marca:"opel", modelo:"corsa"})
+    })
+    .delete((request, response)=>{
+        response.json({message:"borrado"});
+    })
+    .put((request, response)=>{
+        //TODO: Obtener el id y del body para obtener marca y modelo
+        response.json({message:"actualizado"});
+    })
+app.use("/concesionario", routerRest);
+
+
 app.listen(8080);
 console.log("Servidor iniciado");
+
+/*
+Response permite:
+download() end() json() jsonp() redirect() render() send() sendFile() sendStatus()
+*/
