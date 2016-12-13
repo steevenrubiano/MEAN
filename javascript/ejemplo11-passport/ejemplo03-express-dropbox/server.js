b05fb784a1c2f0a32c0f2b59c8d8a83d26a2636c
manana <manana>
2016-12-13 11:30:46 +0100

    act

---
+++ javascript/ejemplo11-passport/ejemplo03-express-dropbox/server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var passport = require('passport');
+var DropboxStrategy = require('passport-dropbox');
+var app = express();
+var session = require('express-session');
+let users = [];
+app.use(passport.initialize());
+app.use(
+    session(
+        {
+            secret: 'clave secreta',
+            resave: false,
+            saveUninitialized: true,
+            cookie:{
+                secure: false
+            }
+        }
+    )
+);
+passport.serializeUser((user, done)=>{
+    //Aqui serializo el usuario
+    done(null, user);
+});
+passport.deserializeUser((user, done)=>{
+    //Aqui deserializo el usuario
+    done(null, user);
+});
+passport.use(new DropboxStrategy({
+    //getaferules@gmx.com
+    //password
+    consumerKey: 'gbvhcgckbjp4nbd',
+    cosumerSecret: 'h28mm0fjalb02e0',
+    callbackUrl: 'http://localhost:8080/home'
+},(token, tokenSecret, profile, next)=>{
+    console.log('Perfil de dropbox: ', profile);
+    if(users[profile.id]){
+        next(null, users[profile.id]);
+    }
+    else{
+        users[profile.id] = profile;
+        next(null, profile);
+    }
+}));
